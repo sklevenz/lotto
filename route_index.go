a3e008c0f992a5bcab4ac53bf4e40b3e2533bede
Stephan Klevenz <stephan@klaeff.de>
2015-02-23 21:04:09 +0100

    first working version

--- route_index.go
+++ route_index.go
@@ -10,18 +10,17 @@ import (
 
 func index(writer http.ResponseWriter, request *http.Request) {
 
-	x := []int{1, 2, 3, 4, 5, 6}
+	x := requestRandom()
 
 	templates := template.Must(template.ParseFiles("template/index.html"))
 	err := templates.Execute(writer, x)
 	if err != nil {
 		log.Fatal(err)
 	}
-	requestRandom()
 
 }
 
-func requestRandom() {
+func requestRandom() []interface{} {
 	url := "https://api.random.org/json-rpc/1/invoke"
 
 	var jsonStr = []byte(`{"jsonrpc":"2.0","method":"generateIntegers","params":{"apiKey":"30dc7b26-988c-49cf-9732-0b24c23d203e","n":6,"min":1,"max":49,"replacement":false},"id":3076}`)
@@ -37,6 +36,18 @@ func requestRandom() {
 
 	var msg interface{}
 	decoder := json.NewDecoder(resp.Body)
-	decoder.Decode(msg)
+	decoder.Decode(&msg)
 	log.Println(msg)
+
+	m := msg.(map[string]interface{})
+	mm := m["result"].(map[string]interface{})
+	mmm := mm["random"].(map[string]interface{})
+	d := mmm["data"].([]interface{})
+
+	log.Println(m)
+	log.Println(mm)
+	log.Println(mmm)
+	log.Println(d)
+
+	return d
 }
